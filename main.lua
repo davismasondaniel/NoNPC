2e8797ee953e11b14f768cb920865a2772ddfad0
DANIELGDM180 <108919916+davismasondaniel@users.noreply.github.com>
2025-04-13 01:34:32 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
 CreateThread(function()
 	while true do
 		Wait(0)
-		SetVehicleDensityMultiplierThisFrame(0.1)
-		SetPedDensityMultiplierThisFrame(0.1)
-		SetRandomVehicleDensityMultiplierThisFrame(0.1)
-		SetScenarioPedDensityMultiplierThisFrame(0.1,0.1)
-		SetParkedVehicleDensityMultiplierThisFrame(0.1)
+		SetVehicleDensityMultiplierThisFrame(0.0)
+		SetPedDensityMultiplierThisFrame(0.0)
+		SetRandomVehicleDensityMultiplierThisFrame(0.0)
+		SetScenarioPedDensityMultiplierThisFrame(0.0,0.0)
+		SetParkedVehicleDensityMultiplierThisFrame(0.0)
 	end
 end)
